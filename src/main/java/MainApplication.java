7b5c32ca0a12532d265dd3f54bafa78d2b61efea
christian-p-anderson <christian.p.anderson1@gmail.com>
2020-12-09 13:53:19 -0500

    added MainApplication class

---
+++ src/main/java/MainApplication.java
@@ -0,0 +1,12 @@
+public class MainApplication {
+    public static void main(String[] args) {
+        DeckOfCards deckOfCards = new DeckOfCards();
+        int count = 0;
+        for (Card card : deckOfCards.getDeck()) {
+            System.out.println(card);
+            count++;
+        }
+        System.out.println("Number of cards in the deck -> " + count);
+        System.out.println("Dealt card -> " + deckOfCards.dealCard());
+    }
+}
